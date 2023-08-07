27ce04314557ce20f6ccdffd8c835a4bd86ed80e
Gerald Bartlett <gerald.bartlett@hotmail.com>
2023-08-06 21:54:39 -0230

    new about & login pages

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 
-ScrollReveal().reveal('.lumber-row');
+
+
+const mobileNav = document.querySelector('.mobile-navigation ul');
+console.log("clicked");
+document.querySelector(".mobile-navigation i").addEventListener("click", () => {
+  mobileNav.classList.contains("show") ? mobileNav.classList.remove("show") : mobileNav.classList.add("show");
+  console.log("mobileNav");
+});
+
+ScrollReveal().reveal(".lumber-row");
