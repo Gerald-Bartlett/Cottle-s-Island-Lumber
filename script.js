3684ed044e0d98524e3b301f78dca869ff87364c
Gerald Bartlett <gerald.bartlett@hotmail.com>
2024-02-04 23:07:20 -0330

    Feb 04, 2024 call me

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ document.querySelector(".mobile-navigation i").addEventListener("click", () => {
 });
 
 ScrollReveal().reveal(".lumber-row");
+
+
+
+function myFunctionLogin() {
+  alert("You are not authorized to login, please retype your credentials");
+}
+
+Webflow.push(function() {
+	$(document).on('submit', 'form', function() {
+    setTimeout(function() { location.reload(true); }, 7000);
+  });
+});
